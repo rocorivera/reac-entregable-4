4bf235983daee247bf1907bf6f7b1df83c05dc3a
rober contreras <trebor.contrerasrivera@gmail.com>
2023-10-30 14:46:37 -0500

    ya

--- src/services/getAllUsers.js
+++ src/services/getAllUsers.js
@@ -2,7 +2,7 @@ import axiosInstance from "../api/axiosInstance";
 
 const getAllUsers = async () => {
   try {
-    const res = await axiosInstance.get("/usersgit ");
+    const res = await axiosInstance.get("/users");
     return res.data;
   } catch (err) {
     console.error(err);
